0f299401dd1994a36c0d7a3991acc2f3f6136b27
kristinesvaboe <kristine@svaboe.com>
2023-05-11 11:04:25 +0200

    Player functionality and models splitting

---
+++ TTScorer/Models/ScoreEntryViewModel.cs
@@ -0,0 +1,9 @@
+namespace TTScorer.Models;
+
+public class ScoreEntryViewModel
+{
+    public int Player1Id { get; set; }
+    public int Player2Id { get; set; }
+    public int Player1Score { get; set; }
+    public int Player2Score { get; set; }
+}
